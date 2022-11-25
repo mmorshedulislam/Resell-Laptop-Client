1959a7c92d17239370813199bf0b30267ec9cd27
MORSHED-HOME <morshed952640@gmail.com>
2022-11-26 01:24:52 +0600

    My orders implement

--- src/Dashboard/MyOrders.js
+++ src/Dashboard/MyOrders.js
@@ -1,9 +1,22 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import Loading from "../Shared/Loading";
 
 const MyOrders = () => {
+  const { user } = useContext(AuthContext);
+  const { data: orders = [], isLoading } = useQuery({
+    queryKey: "orders",
+    queryFn: () =>
+      fetch(`${process.env.REACT_APP_PORT}/bookings`).then((res) => res.json()),
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
-      <h2 className="text-center text-4xl my-5">My Orders</h2>
+      <h2 className="text-center text-4xl my-5">My Orders: {orders?.length}</h2>
 
       <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -27,79 +40,84 @@ const MyOrders = () => {
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-              <td class="p-4 w-32">
-                <img
-                  src="https://flowbite.com/docs/images/products/apple-watch.png"
-                  alt="Apple Watch"
-                />
-              </td>
-              <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
-                Apple Watch
-              </td>
-              <td class="py-4 px-6">
-                <div class="flex items-center space-x-3">
-                  <button
-                    class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                    type="button"
-                  >
-                    <span class="sr-only">Quantity button</span>
-                    <svg
-                      class="w-4 h-4"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
+            {orders.map((order) => (
+              <tr
+                key={order._id}
+                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td class="p-4 w-32">
+                  <img
+                    src="https://flowbite.com/docs/images/products/apple-watch.png"
+                    alt="Apple Watch"
+                  />
+                </td>
+                <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
+                  {order?.productName}
+                </td>
+                <td class="py-4 px-6">
+                  <div class="flex items-center space-x-3">
+                    <button
+                      class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
                     >
-                      <path
-                        fill-rule="evenodd"
-                        d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </button>
-                  <div>
-                    <input
-                      type="number"
-                      id="first_product"
-                      class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="1"
-                      required
-                    />
+                      <span class="sr-only">Quantity button</span>
+                      <svg
+                        class="w-4 h-4"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                    <div>
+                      <input
+                        type="number"
+                        id="first_product"
+                        class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="1"
+                        required
+                      />
+                    </div>
+                    <button
+                      class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
+                    >
+                      <span class="sr-only">Quantity button</span>
+                      <svg
+                        class="w-4 h-4"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
                   </div>
-                  <button
-                    class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white rounded-full border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                    type="button"
+                </td>
+                <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
+                  ${order.price}
+                </td>
+                <td class="py-4 px-6">
+                  <a
+                    href="#"
+                    class="font-medium text-red-600 dark:text-red-500 hover:underline"
                   >
-                    <span class="sr-only">Quantity button</span>
-                    <svg
-                      class="w-4 h-4"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-              </td>
-              <td class="py-4 px-6 font-semibold text-gray-900 dark:text-white">
-                $599
-              </td>
-              <td class="py-4 px-6">
-                <a
-                  href="#"
-                  class="font-medium text-red-600 dark:text-red-500 hover:underline"
-                >
-                  Remove
-                </a>
-              </td>
-            </tr>
+                    Remove
+                  </a>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
